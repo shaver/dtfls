2bc20253461d553555532d17e73f6bb0fcc3dd61
Mike Shaver <shaver@off.net>
2025-12-31 08:36:33 -0500

    add module for making nixosConfigurations from the assembled modules

---
+++ modules/outputs/nixosConfigurations.nix
@@ -0,0 +1,27 @@
+{
+  inputs,
+  config,
+  ...
+}:
+let
+  inherit (config) flake;
+  inherit (inputs) nixpkgs;
+in
+{
+  flake.nixosConfigurations = {
+    # later, this will be a map that assembles all the hosts
+    splashdown =
+      let
+        system = "x86_64-linux";
+        hostname = "splashdown";
+      in
+      nixpkgs.lib.nixosSystem {
+        # these will live in modules/hosts/${hostname}/configuration.nix
+        modules = [ flake.modules.nixosConfigurations.${hostname} ];
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      };
+  };
+}
