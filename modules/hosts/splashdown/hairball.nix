96612cb52056453d2b19c3b8b4a374be0da5774e
Mike Shaver <shaver@off.net>
2026-01-01 12:53:51 -0500

    clean up boilerplate

--- modules/hosts/splashdown/hairball.nix
+++ modules/hosts/splashdown/hairball.nix
@@ -37,15 +37,10 @@
         kernelPackages = pkgs.linuxPackages_latest;
       };
 
-      networking.hostName = "splashdown"; # Define your hostname.
-      # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-      # Configure network proxy if necessary
-      # networking.proxy.default = "http://user:password@proxy:port/";
-      # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-      # Enable networking
-      networking.networkmanager.enable = true;
+      networking = {
+        hostName = "splashdown"; # Define your hostname.
+        networkmanager.enable = true;
+      };
 
       programs.zsh.enable = true;
       # Set your time zone.
@@ -66,12 +61,6 @@
         LC_TIME = "en_CA.UTF-8";
       };
 
-      # Enable CUPS to print documents.
-      # services.printing.enable = true;
-
-      # Enable touchpad support (enabled default in most desktopManager).
-      # services.xserver.libinput.enable = true;
-
       # Define a user account. Don't forget to set a password with ‘passwd’.
       users.users.shaver = {
         isNormalUser = true;
@@ -91,12 +80,8 @@
       # Install firefox.
       programs.firefox.enable = true;
 
-      # List packages installed in system profile. To search, run:
-      # $ nix search wget
       environment.systemPackages = with pkgs; [
-        vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
         git # for flake management
-        #  wget
       ];
 
       programs.neovim = {
@@ -113,24 +98,7 @@
         AllowSuspendThenHibernate=no
       '';
 
-      # Some programs need SUID wrappers, can be configured further or are
-      # started in user sessions.
-      # programs.mtr.enable = true;
-      # programs.gnupg.agent = {
-      #   enable = true;
-      #   enableSSHSupport = true;
-      # };
-
-      # List services that you want to enable:
-
-      # Enable the OpenSSH daemon.
       services.openssh.enable = true;
-
-      # Open ports in the firewall.
-      # networking.firewall.allowedTCPPorts = [ ... ];
-      # networking.firewall.allowedUDPPorts = [ ... ];
-      # Or disable the firewall altogether.
-      # networking.firewall.enable = false;
 
       # This value determines the NixOS release from which the default
       # settings for stateful data, like file locations and database versions
