4f62e551cd64f0abd2b37f27bca93b1177575aea
Mike Shaver <shaver@off.net>
2026-01-02 19:40:25 -0500

    split out the Fastly-specific git config into its own module

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -17,24 +17,33 @@
           branch.autoSetupRebase = "always";
           push.default = "current";
           pull.rebase = "true";
-          url."git@github.com:fastly".insteadOf = "https://github.com/fastly";
-          url."git@github.com:signalsciences".insteadOf = "https://github.com/signalsciences";
         };
         ignores = [
           "*~"
           "*.swp"
         ];
-        includes = [
-          {
-            condition = "gitdir:~/src/**";
-            contents = {
-              user.email = "mike.shaver@fastly.com";
-            };
-          }
-        ];
       };
       lazygit.enable = true;
       jujutsu.enable = true;
+    };
+  };
+
+  flake.modules.homeManager.git-fastly = {
+    programs.git = {
+      settings = {
+        url."git@github.com:fastly".insteadOf = "https://github.com/fastly";
+        url."git@github.com:signalsciences".insteadOf = "https://github.com/signalsciences";
+
+      };
+      includes = [
+        {
+          condition = "gitdir:~/src/**";
+          contents = {
+            user.email = "mike.shaver@fastly.com";
+          };
+        }
+      ];
+
     };
   };
 }
