de3bc6bd2beb78a4fe7c0643441e6d30d2b8fe72
Mike Shaver <shaver@off.net>
2026-01-03 20:55:05 -0500

    resort to lexicographic sort for correct ordering in .ssh/config

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -9,12 +9,16 @@
         enableDefaultConfig = false;
 
         matchBlocks = {
-          "*" = lib.hm.dag.entryBefore [ "github.com" ] {
+          # these are sorted lexicographically by key when emtted into
+          # .ssh/config. lib.hm.dag.* did not work to override that
+          "all" = {
+            host = "*";
             addKeysToAgent = "yes";
             identityFile = "~/.ssh/id_ed25519";
           };
 
-          "github.com" = lib.hm.dag.entryAfter [ "*" ] {
+          "github.com" = {
+            host = "github.com";
             identityFile = "~/.ssh/id_github";
           };
         };
