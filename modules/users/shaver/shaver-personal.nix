729dfd63c3f3eef0462963971b722beb91df20f6
Mike Shaver <shaver@off.net>
2026-01-03 20:22:58 -0500

    add personal authorized_keys on personal systems

--- modules/users/shaver/shaver-personal.nix
+++ modules/users/shaver/shaver-personal.nix
@@ -9,5 +9,10 @@
           config.flake.modules.homeManager.shaver-personal
         ];
       };
+
+      users.users.shaver.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBomlNnFJeNurNUx2v3ciKKfUDXkHI17KFpzj7wUcYrE shaver@shaverbook.local"
+      ];
+
     };
 }
