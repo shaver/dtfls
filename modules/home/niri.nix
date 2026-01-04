bc45d388bd3ccccbc93728172a19801bec5b2d47
Mike Shaver <shaver@off.net>
2026-01-04 16:30:24 -0500

    bring in niri config and add waybar (for now?)

--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -1,12 +1,25 @@
 {
   flake.modules.homeManager.niri =
-    { pkgs, ... }:
+    { pkgs, config, ... }:
+    let
+      configRepo = "${config.home.homeDirectory}/dtfls";
+    in
     {
       home.packages = with pkgs; [
         fuzzel
         alacritty
         hyprlock
+        waybar
         xwayland-satellite
       ];
+
+      # use the "raw" niri config from this repo
+      xdg.configFile = {
+        niri = {
+          source = config.lib.file.mkOutOfStoreSymlink "${configRepo}/config/niri";
+          recursive = true;
+        };
+      };
+
     };
 }
