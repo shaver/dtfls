648c7a1997197e8c21c37f673ce4c66f695b7f07
Mike Shaver <shaver@off.net>
2026-01-06 19:52:08 -0500

    move some general stuff to base-system

--- modules/hosts/splashdown/configuration.nix
+++ modules/hosts/splashdown/configuration.nix
@@ -1,85 +1,24 @@
-{
-  config,
-  inputs,
-  ...
-}:
-{
-  flake.modules.nixos.splashdown =
-    {
-      pkgs,
-      ...
-    }:
-    {
-      imports = [
-        inputs.determinate.nixosModules.default
-      ]
+{ config, inputs, ... }: {
+  flake.modules.nixos.splashdown = { pkgs, ... }: {
+    imports = [ inputs.determinate.nixosModules.default ]
       ++ (with config.flake.modules.nixos; [
+        base-system
         gaming
         desktop-audio
         nix
         desktop
         shaver-personal
         base-networking
-      ])
-      ++ (with config.flake.commonModules; [
-        sudo
-      ]);
+      ]) ++ (with config.flake.commonModules; [ sudo ]);
 
-      # Bootloader.
-      boot = {
-        loader.systemd-boot.enable = true;
-        loader.efi.canTouchEfiVariables = true;
+    powerManagement.enable = true;
+    # sleep crashes this machine, so let's just not
+    systemd.sleep.extraConfig = ''
+      AllowSuspend=no
+      AllowHibernation=no
+      AllowHybridSleep=no
+      AllowSuspendThenHibernate=no
+    '';
 
-        # Use latest kernel.
-        kernelPackages = pkgs.linuxPackages_latest;
-      };
-
-      programs.zsh.enable = true;
-      # Set your time zone.
-      time.timeZone = "America/Toronto";
-
-      # Select internationalisation properties.
-      i18n.defaultLocale = "en_US.UTF-8";
-
-      i18n.extraLocaleSettings = {
-        LC_ADDRESS = "en_CA.UTF-8";
-        LC_IDENTIFICATION = "en_CA.UTF-8";
-        LC_MEASUREMENT = "en_CA.UTF-8";
-        LC_MONETARY = "en_CA.UTF-8";
-        LC_NAME = "en_CA.UTF-8";
-        LC_NUMERIC = "en_CA.UTF-8";
-        LC_PAPER = "en_CA.UTF-8";
-        LC_TELEPHONE = "en_CA.UTF-8";
-        LC_TIME = "en_CA.UTF-8";
-      };
-
-      environment.systemPackages = with pkgs; [
-        git # for flake management
-      ];
-
-      programs.neovim = {
-        enable = true;
-        viAlias = true;
-        vimAlias = true;
-      };
-
-      powerManagement.enable = true;
-      systemd.sleep.extraConfig = ''
-        AllowSuspend=no
-        AllowHibernation=no
-        AllowHybridSleep=no
-        AllowSuspendThenHibernate=no
-      '';
-
-      services.openssh.enable = true;
-
-      # This value determines the NixOS release from which the default
-      # settings for stateful data, like file locations and database versions
-      # on your system were taken. It‘s perfectly fine and recommended to leave
-      # this value at the release version of the first install of this system.
-      # Before changing this value read the documentation for this option
-      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-      system.stateVersion = "25.11"; # Did you read the comment?
-
-    };
+  };
 }
