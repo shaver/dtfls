c661d3d768a5c94e0b9c60a8690d395ebdc07be7
Mike Shaver <shaver@off.net>
2026-01-11 19:53:20 -0500

    get polkit agent working

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -1,10 +1,34 @@
 {
-  flake.modules.nixos.desktop = {
+  flake.modules.nixos.desktop = { pkgs, ... }: {
     # Enable the KDE Plasma Desktop Environment.
     services = {
-      displayManager.sddm.enable = true;
+      displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
       desktopManager.plasma6.enable = true;
     };
+
+    environment.systemPackages = [ pkgs.mate.mate-polkit ];
+    security.polkit.enable = true;
+
+    systemd = {
+      user.services.polkit-mate-1 = {
+        description = "MATE polkit agent";
+        wantedBy = [ "graphical-session.target" ];
+        wants = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart =
+            "${pkgs.mate.mate-polkit}/libexec/polkit-mate-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+      };
+    };
+
     programs.niri.enable = true;
   };
 }
