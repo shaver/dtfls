1b9dd3991e30653a7b1d3290cbdf4823f23b563d
Mike Shaver <shaver@off.net>
2026-01-22 13:27:20 -0500

    less and coreutils to main config

--- modules/users/shaver/home-base.nix
+++ modules/users/shaver/home-base.nix
@@ -1,67 +1,81 @@
-{ inputs, ... }: {
-  flake.modules.homeManager.shaver-base = { pkgs, config, lib, ... }: {
-    imports = with inputs.self.modules.homeManager; [ git neovim shell ssh ];
+{ inputs, ... }:
+{
+  flake.modules.homeManager.shaver-base =
+    {
+      pkgs,
+      config,
+      lib,
+      ...
+    }:
+    {
+      imports = with inputs.self.modules.homeManager; [
+        git
+        neovim
+        shell
+        ssh
+      ];
 
-    home = {
-      username = "shaver";
-      homeDirectory =
-        if pkgs.stdenv.isDarwin then "/Users/shaver" else "/home/shaver";
-      stateVersion = "25.11";
+      home = {
+        username = "shaver";
+        homeDirectory = if pkgs.stdenv.isDarwin then "/Users/shaver" else "/home/shaver";
+        stateVersion = "25.11";
+      };
+
+      programs = {
+        bat.enable = true;
+        jq.enable = true;
+        btop.enable = true;
+        htop.enable = true;
+
+        tmux = {
+          enable = true;
+          shortcut = "a";
+        };
+
+        nh = {
+          enable = true;
+          clean.enable = true;
+          flake = "${config.home.homeDirectory}/dtfls"; # default for "os switch"
+        };
+
+        gh = {
+          enable = true;
+          settings.git_protocol = "https";
+          settings.extensions = [ "yusukebe/gh-markdown-preview" ];
+        };
+        gh-dash.enable = true;
+
+      };
+
+      fonts.fontconfig.enable = true;
+
+      home.packages = with pkgs; [
+        # Unix tools
+        ripgrep # Better `grep`
+        fd
+        sd
+        tree
+        less
+        coreutils
+
+        gnumake
+        clang
+
+        # Nix dev
+        cachix
+        nil # Nix language server
+        nix-info
+        nixpkgs-fmt
+        nixfmt-rfc-style
+
+        jq
+        curl
+        coreutils
+
+        # Fonts
+        nerd-fonts.meslo-lg
+        nerd-fonts.jetbrains-mono
+        font-awesome
+      ];
     };
-
-    programs = {
-      bat.enable = true;
-      jq.enable = true;
-      btop.enable = true;
-      htop.enable = true;
-
-      tmux = {
-        enable = true;
-        shortcut = "a";
-      };
-
-      nh = {
-        enable = true;
-        clean.enable = true;
-        flake = "${config.home.homeDirectory}/dtfls"; # default for "os switch"
-      };
-
-      gh = {
-        enable = true;
-        settings.git_protocol = "https";
-        settings.extensions = [ "yusukebe/gh-markdown-preview" ];
-      };
-      gh-dash.enable = true;
-
-    };
-
-    fonts.fontconfig.enable = true;
-
-    home.packages = with pkgs; [
-      # Unix tools
-      ripgrep # Better `grep`
-      fd
-      sd
-      tree
-
-      gnumake
-      clang
-
-      # Nix dev
-      cachix
-      nil # Nix language server
-      nix-info
-      nixpkgs-fmt
-      nixfmt-rfc-style
-
-      jq
-      curl
-      coreutils
-
-      # Fonts
-      nerd-fonts.meslo-lg
-      nerd-fonts.jetbrains-mono
-      font-awesome
-    ];
-  };
 }
--- modules/users/shaver/home-personal.nix
+++ modules/users/shaver/home-personal.nix
@@ -1,23 +1,40 @@
-{ inputs, ... }: {
-  flake.modules.homeManager.shaver-personal-darwin = { pkgs, ... }: {
-    imports = with inputs.self.modules.homeManager; [ shaver-personal ];
-    home.packages = with pkgs; [ less coreutils ];
-  };
-  flake.modules.homeManager.shaver-personal-nixos-desktop = { pkgs, ... }: {
-    imports = with inputs.self.modules.homeManager; [
-      shaver-personal-nixos
-      niri
-      noctalia
-      desktop
-    ];
-    home.packages = with pkgs; [ signal-desktop ];
-  };
-  flake.modules.homeManager.shaver-personal-nixos = { pkgs, ... }: {
-    imports = with inputs.self.modules.homeManager; [ shaver-personal ];
-  };
-  flake.modules.homeManager.shaver-personal = { flake, pkgs, config, ... }: {
-    imports = with inputs.self.modules.homeManager;
-      [ shaver-base shaver-secrets ]
-      ++ (with inputs.self.modules.homeManager; [ irssi ]);
-  };
+{ inputs, ... }:
+{
+  flake.modules.homeManager.shaver-personal-darwin =
+    { pkgs, ... }:
+    {
+      imports = with inputs.self.modules.homeManager; [ shaver-personal ];
+    };
+  flake.modules.homeManager.shaver-personal-nixos-desktop =
+    { pkgs, ... }:
+    {
+      imports = with inputs.self.modules.homeManager; [
+        shaver-personal-nixos
+        niri
+        noctalia
+        desktop
+      ];
+      home.packages = with pkgs; [ signal-desktop ];
+    };
+  flake.modules.homeManager.shaver-personal-nixos =
+    { pkgs, ... }:
+    {
+      imports = with inputs.self.modules.homeManager; [ shaver-personal ];
+    };
+  flake.modules.homeManager.shaver-personal =
+    {
+      flake,
+      pkgs,
+      config,
+      ...
+    }:
+    {
+      imports =
+        with inputs.self.modules.homeManager;
+        [
+          shaver-base
+          shaver-secrets
+        ]
+        ++ (with inputs.self.modules.homeManager; [ irssi ]);
+    };
 }
