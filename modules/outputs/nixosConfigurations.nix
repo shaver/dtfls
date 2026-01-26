30b7e43118919fc67aaed69230dcd9bd8d6f6752
Mike Shaver <shaver@off.net>
2026-01-26 11:51:40 -0500

    crude cross-compilation config for outpost

--- modules/outputs/nixosConfigurations.nix
+++ modules/outputs/nixosConfigurations.nix
@@ -2,6 +2,17 @@
 let
   inherit (config) flake;
   inherit (inputs) nixpkgs;
+  outpost-arm64-attrs = {
+    modules = [
+      flake.modules.nixos.outpost-arm64
+      { networking.hostName = "outpost-arm64"; }
+    ];
+
+    pkgs = import nixpkgs {
+      system = "aarch64-linux";
+      config.allowUnfree = true;
+    };
+  };
 in {
   flake.nixosConfigurations = {
     # later, this will be a map that assembles all the hosts
@@ -17,16 +28,16 @@ in {
       };
     };
 
-    outpost-arm64 = nixpkgs.lib.nixosSystem {
-      modules = [
-        flake.modules.nixos.outpost-arm64
-        { networking.hostName = "outpost-arm64"; }
-      ];
-
-      pkgs = import nixpkgs {
-        system = "aarch64-linux";
-        config.allowUnfree = true;
-      };
-    };
+    outpost-arm64 = nixpkgs.lib.nixosSystem outpost-arm64-attrs;
+    outpost-arm64_cross-x86_64 = nixpkgs.lib.nixosSystem (outpost-arm64-attrs
+      // {
+        modules = [
+          flake.modules.nixos.outpost-arm64
+          {
+            networking.hostName = "outpost-arm64";
+            nixpkgs.buildPlatform.system = "x86_64-linux";
+          }
+        ];
+      });
   };
 }
