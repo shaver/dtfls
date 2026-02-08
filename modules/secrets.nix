9060770606c7f6cc261feab43c15cc6e8af452fb
Mike Shaver <shaver@off.net>
2026-02-08 17:11:24 -0500

    tidy: old comments

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -4,8 +4,6 @@
       age.keyFile = "/home/shaver/.config/sops/age/keys.txt";
       secrets.ffxiv-otp-secret = {
         sopsFile = ../secrets/users/shaver/secrets.yaml;
-        # owner = "shaver";
-        # group = config.users.users.shaver.group;
       };
     };
   };
