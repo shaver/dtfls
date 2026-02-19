e3d4f1840896d746a8d870508c014c460e0b8605
Mike Shaver <shaver@off.net>
2026-02-19 09:01:39 -0500

    fix work-laptop module path I missed

--- modules/hosts/work-laptop/configuration.nix
+++ modules/hosts/work-laptop/configuration.nix
@@ -1,6 +1,7 @@
 { inputs, ... }: {
   flake.modules.darwin.work-laptop = { pkgs, ... }: {
     imports = with inputs.self.modules.darwin;
-      [ base-system shaver-work ] ++ (with inputs.self.commonModules; [ sudo ]);
+      [ base-system shaver-work ]
+      ++ (with inputs.self.modules.generic; [ sudo ]);
   };
 }
