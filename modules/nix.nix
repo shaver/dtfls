0b58c0c1f41ab64d97a36fb186129704e5f18d85
Mike Shaver <shaver@off.net>
2026-03-04 18:59:54 -0500

    nix: shaver/@wheel as trusted users, re-enable nix generally

--- modules/nix.nix
+++ modules/nix.nix
@@ -2,15 +2,23 @@
   flake.modules.generic.nix =
     { pkgs, ... }:
     {
-      nix.settings.experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
+      nix.settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+
+        trusted-users = [
+          "root"
+          "shaver"
+          "@wheel"
+        ];
+      };
 
       environment.systemPackages = [ pkgs.rippkgs ];
 
       # detsys and nix-darwin don't get along
       # condition on detsys use? mkIf nix.package == blah blah
-      # nix.enable = !pkgs.stdenv.isDarwin;
+      nix.enable = !pkgs.stdenv.isDarwin;
     };
 }
