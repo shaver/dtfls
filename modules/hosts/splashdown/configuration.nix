c8abbcbf5e418cdd79cb5373471117404e252eda
Mike Shaver <shaver@off.net>
2026-03-05 16:36:20 -0500

    splashdown: disable ASPM to maybe resolve igc PCI link issues

--- modules/hosts/splashdown/configuration.nix
+++ modules/hosts/splashdown/configuration.nix
@@ -1,25 +1,35 @@
-{ inputs, ... }: {
-  flake.modules.nixos.splashdown = { pkgs, ... }: {
-    imports = with inputs.self.modules.nixos; [
-      base-system
-      gaming
-      desktop-audio
-      desktop
-      shaver-personal-desktop
-      nvidia-hardware
-      shaver-personal
-      sunshine
-      # testing-kernel
-    ];
+{ inputs, ... }:
+{
+  flake.modules.nixos.splashdown =
+    { pkgs, ... }:
+    {
+      imports = with inputs.self.modules.nixos; [
+        base-system
+        gaming
+        desktop-audio
+        desktop
+        shaver-personal-desktop
+        nvidia-hardware
+        shaver-personal
+        sunshine
+        # testing-kernel
+      ];
 
-    powerManagement.enable = true;
-    # sleep crashes this machine, so let's just not
-    systemd.sleep.extraConfig = ''
-      AllowSuspend=no
-      AllowHibernation=no
-      AllowHybridSleep=no
-      AllowSuspendThenHibernate=no
-    '';
+      powerManagement.enable = true;
 
-  };
+      # disable ASPM due to problem with igc network driver (I225, motherboard)
+      boot.kernelParams = [
+        "pcie_port_pm=off"
+        "pcie_aspm.policy=performance"
+      ];
+
+      # sleep crashes this machine, so let's just not
+      systemd.sleep.extraConfig = ''
+        AllowSuspend=no
+        AllowHibernation=no
+        AllowHybridSleep=no
+        AllowSuspendThenHibernate=no
+      '';
+
+    };
 }
