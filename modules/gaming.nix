dd97b32f33995e66b666cefa6689e3d75be1abe8
Mike Shaver <shaver@off.net>
2026-03-07 11:27:55 -0500

    gaming: remove nix-gaming to debug possible input confusion, I guess

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,48 +1,38 @@
-{ inputs, ... }: {
-  flake.modules.nixos.gaming = { pkgs, ... }:
-    let inherit (inputs.self.packages.${pkgs.stdenv.hostPlatform.system}) xlm;
-    in {
-      imports = with inputs.nix-gaming.nixosModules; [
-        wine
-        platformOptimizations
-      ];
-
-      # avoid having to build everything
-      nix.settings = {
-        substituters = [ "https://nix-gaming.cachix.org" ];
-        trusted-public-keys = [
-          "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        ];
-      };
-
+{ inputs, ... }:
+{
+  flake.modules.nixos.gaming =
+    { pkgs, ... }:
+    let
+      inherit (inputs.self.packages.${pkgs.stdenv.hostPlatform.system}) xlm;
+    in
+    {
       programs.steam = {
         enable = true;
         remotePlay.openFirewall = true;
         localNetworkGameTransfers.openFirewall = true;
         gamescopeSession.enable = true;
-        extraCompatPackages = [ pkgs.proton-ge-bin xlm ];
+        extraCompatPackages = [
+          pkgs.proton-ge-bin
+          xlm
+        ];
         extraPackages = [ pkgs.gamemode ];
-        platformOptimizations.enable = true;
-      };
-
-      programs.wine = {
-        enable = true;
-        ntsync = true;
       };
     };
 
-  flake.modules.homeManager.gaming = { pkgs, lib, config, ... }:
+  flake.modules.homeManager.gaming =
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
     let
       do-ff14-otp = pkgs.writeShellApplication {
         name = "do-ff14-otp";
         text = ''
           for i in $(seq 1 60); do
-            TOTP=$(${
-              lib.getExe' pkgs.oath-toolkit "oathtool"
-            } --totp -b - < ${config.sops.secrets.ffxiv-otp-secret.path})
-            if ${
-              lib.getExe pkgs.curl
-            } -sf "http://localhost:4646/ffxivlauncher/''${TOTP}"; then
+            TOTP=$(${lib.getExe' pkgs.oath-toolkit "oathtool"} --totp -b - < ${config.sops.secrets.ffxiv-otp-secret.path})
+            if ${lib.getExe pkgs.curl} -sf "http://localhost:4646/ffxivlauncher/''${TOTP}"; then
               echo "succeeded with TOTP ''${TOTP} on attempt ''${i}"
               exit 0
             fi
@@ -53,8 +43,12 @@
           exit 1
         '';
       };
-    in {
-      home.packages = [ pkgs.xivlauncher do-ff14-otp ];
+    in
+    {
+      home.packages = [
+        pkgs.xivlauncher
+        do-ff14-otp
+      ];
       xdg.desktopEntries.do-ff14-otp = {
         name = "FF14 OTP";
         exec = "${lib.getExe do-ff14-otp}";
