b728ff196d08d0d9f47558c513ce48f94ace681d
Mike Shaver <shaver@off.net>
2026-04-05 21:16:01 -0400

    noctalia: update and some plugins

--- modules/noctalia.nix
+++ modules/noctalia.nix
@@ -21,12 +21,13 @@
           "gtk-application-prefer-dark-theme" = 1;
         };
 
-        gtk4.extraConfig = {
-          "gtk-application-prefer-dark-theme" = 1;
+        gtk4 = {
+          theme = null;
+          extraConfig = {
+            "gtk-application-prefer-dark-theme" = 1;
+          };
         };
       };
-
-      xdg.configFile."gtk-4.0/gtk.css".force = true;
 
       home.pointerCursor = {
         name = "BreezeX-RosePine-Linux";
