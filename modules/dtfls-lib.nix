b7614fb96067308b938834ae88afd33e6979540d
Mike Shaver <shaver@off.net>
2026-04-22 18:40:54 -0400

    add initial config options for personal/work, laptop/desktop/server

--- modules/dtfls-lib.nix
+++ modules/dtfls-lib.nix
@@ -1,16 +1,35 @@
 { lib, ... }:
 let
   inherit (lib) mkOption;
-  inherit (lib.types) attrs;
+  inherit (lib.types) attrs enum;
 in
 {
   options.flake = {
-    dtflsLib = mkOption {
-      type = attrs;
-      default = { };
-      description = "Library functions for this flake.";
+    dtfls = {
+      opts = {
+        role = {
+          type = enum [
+            "personal"
+            "work"
+          ];
+          default = "personal";
+          description = "Do I get paid to use this computer?";
+        };
+        form = {
+          type = enum [
+            "laptop"
+            "desktop"
+            "server"
+          ];
+          default = "desktop";
+          description = "Can I carry this computer? Should I?";
+        };
+      };
+      lib = mkOption {
+        type = attrs;
+        default = { };
+        description = "Library functions for this flake.";
+      };
     };
   };
-
-  config.flake.dtflsLib.makeBaseHost = hostname: { name = hostname; };
 }
