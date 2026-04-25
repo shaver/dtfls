4112fffd98533dd74b723a339c278cf8936ef40a
Mike Shaver <shaver@off.net>
2026-04-25 11:30:23 -0400

    Add CUDA-enabled caches because compiling that stuff sucks.
    mkDefault for nvidiaPackages for easier overriding.

--- modules/nvidia-hardware.nix
+++ modules/nvidia-hardware.nix
@@ -1,16 +1,25 @@
 {
   flake.modules.nixos.nvidia-hardware =
-    { config, ... }:
+    { config, lib, ... }:
+    let
+      inherit (lib) mkDefault;
+    in
     {
       hardware.graphics.enable = true; # enable OpenGL
       services.xserver.videoDrivers = [ "nvidia" ];
       hardware.nvidia = {
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        package = mkDefault config.boot.kernelPackages.nvidiaPackages.stable;
         modesetting.enable = true;
         powerManagement.enable = false;
         powerManagement.finegrained = false;
         open = true; # open-source NVIDIA kernel module (not "nouveau"!)
         nvidiaSettings = true;
+      };
+
+      # don't compile the CUDA stuff if we don't have to
+      nix.settings = {
+        extra-substituters = [ "https://cache.nixos-cuda.org" ];
+        extra-trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
       };
     };
 }
