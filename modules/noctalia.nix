2cc8f1378432c8345956825b676196472729bdcc
Mike Shaver <shaver@off.net>
2026-07-09 15:22:09 -0400

    flake: update
    explicitly enable home.pointerCursor per deprecation warning

--- modules/noctalia.nix
+++ modules/noctalia.nix
@@ -34,11 +34,11 @@
       };
 
       home.pointerCursor = {
+        enable = true;
         name = "BreezeX-RosePine-Linux";
         package = pkgs.rose-pine-cursor;
         size = 24;
         gtk.enable = true;
-        x11.enable = true;
       };
 
       home.sessionVariables = {
